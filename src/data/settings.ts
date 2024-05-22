d3e4d6c128d8321a8715b2a82d27eac5df3745ee
Zaïda Rivai <zaidarivai@Zaidas-MacBook-Pro.local>
2024-05-22 22:17:04 +0200

    update seo

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
   site: 'https://cannonball.littlesticks.dev',
   name: 'Little Sticks',
-  title: 'Cannonball by Little Sticks | A splash page template',
-  description: 'This is a simple splash page template built with Astro by Little Sticks',
+  title: 'HormoNova | By Women For Women',
+  description: 'Predict, prepare, and empower, continuous hormone insights for every woman.',
 }
